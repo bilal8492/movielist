968a0aa3f874ab7aafdb38797ca896392cc51f89
Ansari Bilal <m.bilal8492@gmail.com>
2021-02-16 22:56:35 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./Components/MoviesList/Movies";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
+
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Movies />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <switch>
+          <Route path='/abc'>
+            <Movies />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <SignUp />
+          </Route>
+        </switch>
+
+
+
+
+
+      </div>
+    </BrowserRouter>
   );
 }
 
